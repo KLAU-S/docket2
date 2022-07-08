bd320af4a019ce3558cd90fef136615835fe7271
Enayat Kareem <75158477+KLAU-S@users.noreply.github.com>
2022-07-08 23:12:40 +0530

    Update settings.py

--- convinAss/docket/settings.py
+++ convinAss/docket/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v!9r7g^1aq)i6_w3x*%y^dsvcdwe&d=q^$0mo%byd7mj0ftlhs'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
